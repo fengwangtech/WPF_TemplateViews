3dc0fc3ff29ff6216edf7e9eb8ac0709c2734669
fengwangtech <fengwangtech@gmail.com>
2024-07-13 10:21:43 -0500

    first commit

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Input;
+
+
+namespace WPF_TemplateViews.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel = new AboutViewModel();
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Home")
+            {
+                viewModel.SelectedViewModel = new HomeViewModel();
+            }
+            else if (parameter.ToString() == "About")
+            {
+                viewModel.SelectedViewModel = new AboutViewModel();
+            }
+        }
+    }
+}
